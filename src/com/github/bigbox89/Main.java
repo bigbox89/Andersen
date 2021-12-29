c819c5e45da93df13a3d157b46e8e226c7587652
Eduard <e9047689558@yandex.ru>
2021-12-29 21:40:17 +0300

    test commit

---
+++ src/com/github/bigbox89/Main.java
@@ -0,0 +1,10 @@
+package com.github.bigbox89;
+
+public class Main {
+
+    public static void main(String[] args)
+    {
+        System.out.println("Till tomorrow");
+        // write your code here
+    }
+}
