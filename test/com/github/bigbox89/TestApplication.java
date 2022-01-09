63eea25083159a6785a3ac82e372ed1a4e53df50
petr pyatkov <bigbox89@mail.ru>
2022-01-09 21:38:46 +0500

    add test methods for method sort() in arrayList class

--- test/com/github/bigbox89/TestApplication.java
+++ test/com/github/bigbox89/TestApplication.java
@@ -15,21 +15,31 @@ public class TestApplication {
     }
 
     @Test
+    public void sortTest(){
+        assertEquals(list.size(), 0);
+        fillList();
+        list.sort();
+        assertEquals(list.get(0), "test5" );
+        assertEquals(list.get(1), "test4" );
+        assertEquals(list.get(2), "test3" );
+        assertEquals(list.get(3), "test2" );
+        assertEquals(list.get(4), "test1" );
+    }
+    @Test
     public void sizeTest() {
         assertEquals(list.size(), 0);
         fillList();
         assertEquals(list.size(), 5);
     }
 
-
     @Test
     public void addTest(){
         assertEquals(list.size(), 0);
-        list.add("first");
-        list.add("Second");
-        list.add("Third");
-        list.add("Fourth");
-        list.add("Fifth");
+        list.add("test1");
+        list.add("test2");
+        list.add("test3");
+        list.add("test4");
+        list.add("test5");
         assertEquals(list.size(), 5);
     }
 
@@ -37,13 +47,9 @@ public class TestApplication {
     public void removeTest() {
         fillList();
         assertEquals(list.size(), 5);
-
         list.remove(1);
-
         assertEquals(list.size(), 4);
-
-        list.remove("first");
-
+        list.remove("test3");
         assertEquals(list.size(), 3);
     }
 
@@ -58,14 +64,14 @@ public class TestApplication {
     @Test
     public void getTest() {
         fillList();
-        assertEquals(list.get(1), "Second");
+        assertEquals(list.get(1), "test2");
     }
 
     private void fillList() {
-        list.add("first");
-        list.add("Second");
-        list.add("Third");
-        list.add("Fourth");
-        list.add("Fifth");
+        list.add("test1");
+        list.add("test2");
+        list.add("test3");
+        list.add("test4");
+        list.add("test5");
     }
 }
